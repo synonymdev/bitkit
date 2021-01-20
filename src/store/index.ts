9a8448519633746d37dd322bd8080b64538ff2cf
Jason van den Berg <jayvdb1@gmail.com>
2021-01-20 09:45:55 +0200

    Switching off logging for unit tests and prod environment

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,7 +5,13 @@ import logger from 'redux-logger';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const { persistReducer } = require('redux-persist');
-const createStoreWithMiddleware = applyMiddleware(thunk, logger)(createStore);
+
+//Switch off logging for unit tests and prod env
+const createStoreWithMiddleware = (process.env.JEST_WORKER_ID === undefined &&
+	__DEV__
+	? applyMiddleware(thunk, logger)
+	: applyMiddleware(thunk))(createStore);
+
 const persistConfig = {
 	key: 'root',
 	storage: AsyncStorage,
