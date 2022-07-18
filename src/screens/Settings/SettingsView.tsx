c80f894c57ce29c9c2bd661e52b6871b6e88790d
Miguel Medeiros <miguel@miguelmedeiros.com.br>
2022-07-18 10:57:29 -0300

    fix(settngs): background color

--- src/screens/Settings/SettingsView.tsx
+++ src/screens/Settings/SettingsView.tsx
@@ -65,10 +65,8 @@ const SettingsView = ({
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: 'rgba(255, 255, 255, 0)',
 	},
 	listContent: {
-		backgroundColor: 'rgba(255, 255, 255, 0)',
 		paddingHorizontal: 16,
 		flex: 1,
 	},
