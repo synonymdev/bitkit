a674e1d3f5cd7217165b373d41cc3eb774cf0790
Philipp Walter <philippwalter@proton.me>
2022-09-15 12:19:34 +0200

    fix(scan): update camera permission screen

--- src/components/Camera.tsx
+++ src/components/Camera.tsx
@@ -2,10 +2,10 @@ import React, { ReactElement, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { useIsFocused } from '@react-navigation/native';
 import { RNCamera } from 'react-native-camera';
-import { systemWeights } from 'react-native-typography';
 
-import { EvilIcon, Text, View } from '../styles/components';
+import { View } from '../styles/components';
 import { showErrorNotification } from '../utils/notifications';
+import CameraNoAuth from './CameraNoAuth';
 
 const Camera = ({
 	onBarCodeRead = (): null => null,
@@ -20,18 +20,6 @@ const Camera = ({
 }): ReactElement => {
 	const isFocused = useIsFocused();
 	const [_data, setData] = useState('');
-	const notAuthorizedView = (
-		<View style={styles.notAuthorizedView}>
-			<EvilIcon name={'exclamation'} size={60} />
-			<Text style={styles.boldText}>
-				It appears Bitkit does not have permission to access your camera.
-			</Text>
-			<Text style={styles.text}>
-				To utilize this feature in the future you will need to enable camera
-				permissions for this app from your phones settings.
-			</Text>
-		</View>
-	);
 
 	const onMountError = (): void => {
 		console.error(
@@ -60,7 +48,7 @@ const Camera = ({
 						}
 					}}
 					onMountError={onMountError}
-					notAuthorizedView={notAuthorizedView}
+					notAuthorizedView={<CameraNoAuth />}
 					type={RNCamera.Constants.Type.back}
 					flashMode={
 						flashMode
@@ -93,25 +81,6 @@ const styles = StyleSheet.create({
 	},
 	content: {
 		flex: 1,
-	},
-	notAuthorizedView: {
-		flex: 1,
-		top: -40,
-		backgroundColor: 'transparent',
-		alignItems: 'center',
-		justifyContent: 'center',
-		paddingHorizontal: 20,
-	},
-	text: {
-		...systemWeights.regular,
-		fontSize: 18,
-		textAlign: 'center',
-	},
-	boldText: {
-		...systemWeights.bold,
-		fontSize: 18,
-		textAlign: 'center',
-		marginVertical: 10,
 	},
 });
 
---
+++ src/components/CameraNoAuth.tsx
@@ -0,0 +1,47 @@
+import React, { ReactElement } from 'react';
+import { StyleSheet } from 'react-native';
+import { systemWeights } from 'react-native-typography';
+
+import { EvilIcon, Text, View } from '../styles/components';
+import NavigationHeader from './NavigationHeader';
+import SafeAreaView from './SafeAreaView';
+
+const CameraNoAuth = (): ReactElement => {
+	return (
+		<SafeAreaView>
+			<NavigationHeader title="Permission" />
+			<View style={styles.container} color="black">
+				<EvilIcon name="exclamation" size={60} />
+				<Text style={styles.boldText}>
+					It appears Bitkit does not have permission to access your camera.
+				</Text>
+				<Text style={styles.text}>
+					To utilize this feature in the future you will need to enable camera
+					permissions for this app from your phones settings.
+				</Text>
+			</View>
+		</SafeAreaView>
+	);
+};
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+		paddingHorizontal: 16,
+	},
+	text: {
+		...systemWeights.regular,
+		fontSize: 18,
+		textAlign: 'center',
+	},
+	boldText: {
+		...systemWeights.bold,
+		fontSize: 18,
+		textAlign: 'center',
+		marginVertical: 10,
+	},
+});
+
+export default CameraNoAuth;
