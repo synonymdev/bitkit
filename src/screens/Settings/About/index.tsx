5bc57abf6338e2537dfcc53a9abaf535f2e7dc63
Philipp Walter <philippwalter@proton.me>
2022-10-26 12:55:58 +0200

    fix(settings): update About

--- src/screens/Settings/About/index.tsx
+++ src/screens/Settings/About/index.tsx
@@ -1,10 +1,10 @@
 import React, { memo, ReactElement, useMemo } from 'react';
-import Rate, { AndroidMarket } from 'react-native-rate';
+// import Rate, { AndroidMarket } from 'react-native-rate';
 import {
 	Text,
 	View,
 	FlatList,
-	Share,
+	// Share,
 	Image,
 	Platform,
 	StyleSheet,
@@ -37,67 +37,64 @@ const AboutSettings = ({ navigation }): ReactElement => {
 			? `https://apps.apple.com/us/app/bitkit/id${appleAppID}`
 			: `https://play.google.com/store/apps/details?id=${androidPackageName}`;
 
+	// TODO: uncomment links after full launch
+
 	const SettingsListData: IListData[] = useMemo(
 		() => [
 			{
 				data: [
-					{
-						title: 'Leave a review',
-						type: 'button',
-						onPress: (): void => {
-							const options = {
-								AppleAppID: appleAppID,
-								GooglePackageName: androidPackageName,
-								// OtherAndroidURL: 'http://www.randomappstore.com/app/47172391',
-								preferredAndroidMarket: AndroidMarket.Google,
-								preferInApp: Platform.OS !== 'android',
-								openAppStoreIfInAppFails: true,
-								fallbackPlatformURL: 'https://www.synonym.to/',
-							};
-							Rate.rate(options, (success, _errorMessage) => {
-								if (success) {
-									// TODO: show thank you message
-								}
-							});
-						},
-						hide: false,
-					},
-					{
-						title: 'Frequently Asked Questions',
-						type: 'button',
-						onPress: (): void => {
-							// TODO: update with correct url
-							openURL('https://www.synonym.to/').then();
-						},
-						hide: false,
-					},
+					// {
+					// 	title: 'Leave a review',
+					// 	type: 'button',
+					// 	onPress: (): void => {
+					// 		const options = {
+					// 			AppleAppID: appleAppID,
+					// 			GooglePackageName: androidPackageName,
+					// 			// OtherAndroidURL: 'http://www.randomappstore.com/app/47172391',
+					// 			preferredAndroidMarket: AndroidMarket.Google,
+					// 			preferInApp: Platform.OS !== 'android',
+					// 			openAppStoreIfInAppFails: true,
+					// 			fallbackPlatformURL: 'https://www.bitkit.to/',
+					// 		};
+					// 		Rate.rate(options, (success, _errorMessage) => {
+					// 			if (success) {
+					// 				// TODO: show thank you message
+					// 			}
+					// 		});
+					// 	},
+					// },
+					// {
+					// 	title: 'Frequently Asked Questions',
+					// 	type: 'button',
+					// 	onPress: (): void => {
+					// 		// TODO: update with correct url
+					// 		openURL('https://www.synonym.to/').then();
+					// 	},
+					// },
 					{
 						title: 'Report a bug or contribute',
 						type: 'button',
 						onPress: (): void => {
 							openURL('https://github.com/synonymdev/bitkit').then();
 						},
-						hide: false,
 					},
-					{
-						title: 'Share Bitkit with a friend',
-						type: 'button',
-						onPress: async (): Promise<void> => {
-							await Share.share({
-								title: 'Bitkit',
-								message: `Download Bitkit, Your Ultimate Bitcoin Toolkit. Handing you the keys to reshape your digital life. ${appStoreUrl}`,
-							});
-						},
-						hide: false,
-					},
+					// {
+					// 	title: 'Share Bitkit with a friend',
+					// 	type: 'button',
+					// 	onPress: async (): Promise<void> => {
+					// 		await Share.share({
+					// 			title: 'Bitkit',
+					// 			message: `Download Bitkit, Your Ultimate Bitcoin Toolkit. Handing you the keys to reshape your digital life. ${appStoreUrl}`,
+					// 		});
+					// 	},
+					// },
 					{
 						title: 'Legal',
 						type: 'button',
 						onPress: (): void => {
 							// TODO: update with correct url
-							openURL('https://www.synonym.to/').then();
+							openURL('https://bitkit.to/terms-of-use').then();
 						},
-						hide: false,
 					},
 					{
 						title: 'Version',
@@ -106,7 +103,6 @@ const AboutSettings = ({ navigation }): ReactElement => {
 						onPress: (): void => {
 							openURL('https://github.com/synonymdev/bitkit/releases').then();
 						},
-						hide: false,
 					},
 				],
 			},
@@ -138,66 +134,62 @@ const AboutSettings = ({ navigation }): ReactElement => {
 		</SettingsView>
 	);
 
-	const footerComponent = (
-		<View style={styles.container}>
-			<View style={styles.logoContainer}>
-				<Pressable
-					style={styles.logoLink}
-					onPress={(): void => {
-						navigation.navigate('EasterEgg');
-					}}
-				/>
-				<BitkitIcon height={64} width={184} />
-				<Image style={styles.poweredBy} source={imageSrc} />
-			</View>
-			<View style={styles.containerSocial}>
-				<EmailIcon
-					onPress={(): void => {
-						openURL('mailto:info@synonym.to?subject=Bitkit');
-					}}
-					height={24}
-					width={24}
-				/>
-				<GlobeIcon
-					onPress={(): void => {
-						openURL('https://bitkit.to');
-					}}
-					height={24}
-					width={24}
-				/>
-				<TwitterIcon
-					onPress={(): void => {
-						openURL('https://twitter.com/bitkitwallet');
-					}}
-					height={24}
-					width={24}
-				/>
-				<MediumIcon
-					onPress={(): void => {
-						openURL('https://medium.com/synonym-to');
-					}}
-					height={24}
-					width={24}
-				/>
-				<GithubIcon
-					onPress={(): void => {
-						openURL('https://github.com/synonymdev');
-					}}
-					height={24}
-					width={24}
-				/>
-			</View>
-		</View>
-	);
-
 	return (
 		<GlowingBackground bottomRight="#FF6600">
 			<FlatList
 				data={null}
 				renderItem={null}
 				ListHeaderComponent={headerComponent}
-				ListFooterComponent={footerComponent}
 			/>
+			<View style={[styles.footer]}>
+				<View style={styles.logoContainer}>
+					<Pressable
+						style={styles.logoLink}
+						onPress={(): void => {
+							navigation.navigate('EasterEgg');
+						}}
+					/>
+					<BitkitIcon height={64} width={184} />
+					<Image style={styles.poweredBy} source={imageSrc} />
+				</View>
+				<View style={styles.containerSocial}>
+					<EmailIcon
+						onPress={(): void => {
+							openURL('mailto:info@synonym.to?subject=Bitkit');
+						}}
+						height={24}
+						width={24}
+					/>
+					<GlobeIcon
+						onPress={(): void => {
+							openURL('https://bitkit.to');
+						}}
+						height={24}
+						width={24}
+					/>
+					<TwitterIcon
+						onPress={(): void => {
+							openURL('https://twitter.com/bitkitwallet');
+						}}
+						height={24}
+						width={24}
+					/>
+					<MediumIcon
+						onPress={(): void => {
+							openURL('https://medium.com/synonym-to');
+						}}
+						height={24}
+						width={24}
+					/>
+					<GithubIcon
+						onPress={(): void => {
+							openURL('https://github.com/synonymdev');
+						}}
+						height={24}
+						width={24}
+					/>
+				</View>
+			</View>
 		</GlowingBackground>
 	);
 };
@@ -205,22 +197,32 @@ const AboutSettings = ({ navigation }): ReactElement => {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		flexDirection: 'column',
 		alignItems: 'center',
 		marginTop: 48,
 	},
-	containerSocial: {
-		width: 300,
-		flexDirection: 'row',
+	textIntro: {
+		fontStyle: 'normal',
+		fontWeight: '400',
+		fontSize: 17,
+		lineHeight: 22,
 		alignItems: 'center',
-		justifyContent: 'space-evenly',
-		marginBottom: 32,
+		letterSpacing: -0.4,
+		color: '#8E8E93',
+		paddingLeft: 16,
+		paddingRight: 16,
+		paddingBottom: 12,
+		paddingTop: 12,
+	},
+	footer: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'flex-end',
+		marginTop: 'auto',
 	},
 	logoContainer: {
 		position: 'relative',
 		marginBottom: 42,
 		justifyContent: 'center',
-		display: 'flex',
 		alignItems: 'center',
 	},
 	logoLink: {
@@ -230,19 +232,12 @@ const styles = StyleSheet.create({
 		height: 65,
 		width: 65,
 	},
-	textIntro: {
-		fontStyle: 'normal',
-		fontWeight: '400',
-		fontSize: 17,
-		lineHeight: 22,
-		display: 'flex',
+	containerSocial: {
+		width: 300,
+		flexDirection: 'row',
 		alignItems: 'center',
-		letterSpacing: -0.4,
-		color: '#8E8E93',
-		paddingLeft: 16,
-		paddingRight: 16,
-		paddingBottom: 12,
-		paddingTop: 12,
+		justifyContent: 'space-evenly',
+		marginBottom: 32,
 	},
 	poweredBy: {
 		position: 'absolute',
