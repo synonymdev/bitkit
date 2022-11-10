422c902559baa266d955e25f061c4fc3330b91c5
Philipp Walter <philippwalter@proton.me>
2022-11-10 17:57:07 +0100

    chore: ignore LogBox warning (temp)

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { AppRegistry, Text, TextInput } from 'react-native';
+import { AppRegistry, LogBox, Text, TextInput } from 'react-native';
 import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
 
 import Root from './Root';
@@ -9,5 +9,8 @@ Text.defaultProps = Text.defaultProps || {};
 Text.defaultProps.allowFontScaling = false;
 TextInput.defaultProps = TextInput.defaultProps || {};
 TextInput.defaultProps.allowFontScaling = false;
+
+// TEMP: ignore <Dialog /> warning on iOS
+LogBox.ignoreLogs(['Modal with']);
 
 AppRegistry.registerComponent(appName, () => gestureHandlerRootHOC(Root));
