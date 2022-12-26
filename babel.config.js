a30f2807f305f25791f96e05f2416551feb53926
Ivan Vershigora <ivan.vershigora@gmail.com>
2022-12-26 16:44:37 +0300

    fix: run jest tests on ubuntu

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	presets: ['module:metro-react-native-babel-preset'],
-	plugins: ['react-native-reanimated/plugin', 'module:react-native-dotenv'],
+	plugins: ['module:react-native-dotenv', 'react-native-reanimated/plugin'],
 	env: {
 		production: {
 			plugins: ['transform-remove-console'],
