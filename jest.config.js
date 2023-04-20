676e3611d9b9578c39419e3b44cd80a6356c61f6
Ivan Vershigora <ivan.vershigora@gmail.com>
2023-04-20 17:17:35 +0300

    fix: downgrade node-fetch to fix jest tests

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	preset: 'react-native',
 	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-	transformIgnorePatterns: ['<roodDir>/__mocks__'],
+	transformIgnorePatterns: ['<roodDir>/__mocks__', 'jest-runner'],
 	setupFiles: [
 		'<rootDir>/jest.setup.js',
 		'./node_modules/react-native-gesture-handler/jestSetup.js',
