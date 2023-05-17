d3b0a75fe29f81b12a6829aae863893a5d4b4d52
Ivan Vershigora <ivan.vershigora@gmail.com>
2023-05-17 16:07:20 +0100

    test: buildcache

--- __tests__/wallet.ts
+++ __tests__/wallet.ts
@@ -1,10 +1,15 @@
+import * as bip39 from 'bip39';
+
 // Fix 'getDispatch is not a function'
 import '../src/store/actions/ui';
-import { deriveMnemonicPhrases } from '../src/utils/wallet';
+import {
+	deriveMnemonicPhrases,
+	slashtagsPrimaryKey,
+} from '../src/utils/wallet';
 import { mnemonic } from './utils/dummy-wallet';
 
 describe('Wallet Methods', () => {
-	it('Derive multiple mnemonic phrases for lightning, tokens and slashtags via the on-chain phrase.', async () => {
+	it('Derive multiple mnemonic phrases for lightning and tokens via the on-chain phrase.', async () => {
 		const res = await deriveMnemonicPhrases(mnemonic);
 		if (res.isErr()) {
 			expect(res.error.message).toEqual('');
@@ -16,6 +21,14 @@ describe('Wallet Methods', () => {
 		);
 		expect(res.value.tokens).toEqual(
 			'inspire sick rule wild near rebuild pride tomato shell come drip reduce street steel warrior project radar sister day title spice execute evolve outside',
+		);
+	});
+
+	it('Derive slashtags primay key from the Seed', async () => {
+		const seed = await bip39.mnemonicToSeed(mnemonic);
+		const slashtags = await slashtagsPrimaryKey(seed);
+		expect(slashtags).toEqual(
+			'be6d74439798f3217043a38de8c4da5a7e2b73714ace0811163105731ff0a066',
 		);
 	});
 });
--- postinstall.js
+++ postinstall.js
@@ -7,27 +7,21 @@ const createDevEnvFile = 'cp .env.development.template .env.development';
 
 //Create development environment file if it doesn't exist
 fs.access(devEnvFile, fs.constants.F_OK, (err) => {
-	if (err) {
+	if (err && !process.env.CI) {
 		console.log(`File '${devEnvFile}' not found, running createDevEnv...`);
 		exec(createDevEnvFile);
 	}
 });
 
-const baseCommand = `
-cd nodejs-assets/nodejs-project &&
-yarn install &&
-cd ../../ &&
+let baseCommand = `
+yarn install --no-audit --prefer-offline --production=true --cwd nodejs-assets/nodejs-project &&
 rn-nodeify --install buffer,stream,assert,events,crypto,vm,process --hack`;
 
-function postInstallMac() {
-	exec(`${baseCommand} && react-native setup-ios-permissions && cd ios && pod install && cd ..`);
-}
-function postInstallLinWin() {
-	exec(baseCommand);
+if (os.type() === 'Darwin') {
+	baseCommand  += '&& react-native setup-ios-permissions';
+	if (!process.env.CI) {
+		baseCommand += '&& pod install --project-directory=ios';
+	}
 }
 
-if (os.type() === 'Darwin') {
-	postInstallMac();
-} else {
-	postInstallLinWin();
-}
+exec(baseCommand);
