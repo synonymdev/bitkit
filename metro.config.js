2ec5a298d93ead3e60575f255415873c653dc92d
Philipp Walter <philippwalter@proton.me>
2023-09-12 17:14:57 +0200

    chore: remove native libsodium

--- metro.config.js
+++ metro.config.js
@@ -7,8 +7,6 @@
 const { getDefaultConfig } = require('metro-config');
 const exclusionList = require('metro-config/src/defaults/exclusionList');
 
-const path = require('path');
-
 module.exports = (async () => {
   const {
     resolver: { sourceExts, assetExts },
@@ -26,13 +24,9 @@ module.exports = (async () => {
     resolver: {
       assetExts: assetExts.filter((ext) => ext !== 'svg'),
       sourceExts: [...sourceExts, 'svg'],
-      extraNodeModules: {
-        "sodium-native": path.resolve(__dirname, './node_modules/react-native-libsodium'),
-      },
       blacklistRE: exclusionList([
         /android\/build\/nodejs-native-assets-temp-build\/.*/,
         /\/nodejs-assets\/.*/,
-        /\/node_modules\/sodium-native\/.*/,
         /\/android\/build\/*/,
       ])
     },
