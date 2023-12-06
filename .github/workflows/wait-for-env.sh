d722f12bfb69da8b177fca8b36e8a5a02dc76851
Ivan Vershigora <ivan.vershigora@gmail.com>
2023-12-06 17:51:40 +0000

    fix: move colima logic to script file

---
+++ .github/workflows/wait-for-env.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# wait for the colima env to be ready, restart if not ready after 60 seconds
+
+ATTEMPTS=30
+
+while true
+do
+  echo "::group::check ports"
+
+  echo -n "Bitcoin port 43782 "
+  bitcoin=false
+  for i in $(seq 1 $ATTEMPTS)
+  do
+    nc -z '127.0.0.1' 43782 > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+      echo " success!"
+      bitcoin=true
+      break
+    fi
+    echo -n "."
+    sleep 1
+  done
+
+  if [ "$bitcoin" = true ] ; then
+    echo -n "electrum port 60001 "
+    for i in $(seq 1 $ATTEMPTS)
+    do
+      nc -z '127.0.0.1' 60001 > /dev/null 2>&1
+      if [ $? -eq 0 ]; then
+        echo " success!"
+        echo "::endgroup::"
+        exit 0
+      fi
+      echo -n "."
+      sleep 1
+    done
+  fi
+
+  echo " failed!"
+  echo "::endgroup::"
+
+  echo "::group::colima restart"
+  colima restart
+  echo "::endgroup::"
+done
+
