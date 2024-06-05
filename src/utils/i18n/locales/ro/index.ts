4ecdc9bc4842bb34605c2ebe71fbb2781f3d57b1
Ivan Vershigora <ivan.vershigora@gmail.com>
2024-06-05 13:28:36 +0100

    fix: sync language files

---
+++ src/utils/i18n/locales/ro/index.ts
@@ -0,0 +1,23 @@
+import cards from './cards.json';
+import common from './common.json';
+import fee from './fee.json';
+import lightning from './lightning.json';
+import onboarding from './onboarding.json';
+import other from './other.json';
+import security from './security.json';
+import settings from './settings.json';
+import slashtags from './slashtags.json';
+import wallet from './wallet.json';
+
+export default {
+	cards,
+	common,
+	fee,
+	lightning,
+	onboarding,
+	other,
+	security,
+	settings,
+	slashtags,
+	wallet,
+};
