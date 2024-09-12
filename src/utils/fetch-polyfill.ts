882f0e364c63897dd8098a73c3a71504484a0dfb
Philipp Walter <philippwalter@proton.me>
2024-09-12 12:10:29 +0200

    chore: remove unused dependencies

--- src/utils/fetch-polyfill.ts
+++ src/utils/fetch-polyfill.ts
@@ -1,5 +1,5 @@
 // Polyfill fetch text streaming
-// NOTE: TextEncoder is polyfilled in shim.js
+// NOTE: TextDecoder is polyfilled in shim.js
 
 import { polyfill as polyfillFetch } from 'react-native-polyfill-globals/src/fetch';
 import { polyfill as polyfillReadableStream } from 'react-native-polyfill-globals/src/readable-stream';
